ad102eb19ff7ec151fdfd0c35d87ee2cbb40c4ac
iftekhar-ahmed <bit0220iftekhar@gmail.com>
2014-12-11 20:06:45 +0600

    Add dial.

--- app/src/main/java/com/apptitive/glsurfaceclock/view/ClockView.java
+++ app/src/main/java/com/apptitive/glsurfaceclock/view/ClockView.java
@@ -3,9 +3,9 @@ package com.apptitive.glsurfaceclock.view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.CornerPathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
 import android.graphics.Point;
 import android.os.Build;
 import android.util.AttributeSet;
@@ -18,11 +18,14 @@ import android.view.WindowManager;
  */
 public class ClockView extends View {
 
-    private Paint circlePaint;
     private Paint dialPaint;
-    private Paint paint;
     private Point centerPoint;
-    private Path dialPath;
+    private Path dialPathShape;
+
+    private final float SEC_HOUR_MARK_RADIUS = 3;
+    private final float DIAL_RADIUS = 200;
+    private final float ADVANCE = (2 * (float)Math.PI * DIAL_RADIUS) / 60 - (SEC_HOUR_MARK_RADIUS / 2) + 0.15f;
+    private final float PHASE = ADVANCE;
 
     public ClockView(Context context) {
         super(context);
@@ -41,37 +44,28 @@ public class ClockView extends View {
 
     private void init() {
         setBackgroundColor(Color.RED);
-        circlePaint = new Paint();
-        circlePaint.setStyle(Paint.Style.FILL);
-        circlePaint.setColor(Color.BLACK);
-        circlePaint.setAntiAlias(true);
         centerPoint = new Point();
-        dialPath = new Path();
-        dialPath.addCircle(250, 350, 20, Path.Direction.CW);
-        dialPath.addCircle(260, 360, 20, Path.Direction.CW);
-        dialPath.addRect(260, 370, 360, 470, Path.Direction.CW);
-        paint = new Paint();
-        paint.setColor(Color.BLACK);                    // set the color
-        paint.setStrokeWidth(10);               // set the size
-        paint.setDither(true);                    // set the dither to true
-        paint.setStyle(Paint.Style.STROKE);       // set to STOKE
-        paint.setStrokeJoin(Paint.Join.ROUND);    // set the join to round you want
-        paint.setStrokeCap(Paint.Cap.ROUND);      // set the paint cap to round too
-        paint.setPathEffect(new CornerPathEffect(10) );   // set the path effect when they join.
-        paint.setAntiAlias(true);
         Display display = ((WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
             display.getSize(centerPoint);
-            centerPoint.set(centerPoint.x / 3, centerPoint.y / 3);
+            centerPoint.set(centerPoint.x / 2, centerPoint.y / 2);
         } else {
-            centerPoint.set(display.getWidth() / 3, display.getHeight() / 3);
+            centerPoint.set(display.getWidth() / 2, display.getHeight() / 2);
         }
+        dialPathShape = new Path();
+        dialPathShape.addCircle(0, 0, SEC_HOUR_MARK_RADIUS, Path.Direction.CW);
+        dialPaint = new Paint();
+        dialPaint.setStyle(Paint.Style.STROKE);
+        dialPaint.setStrokeWidth(5);
+        dialPaint.setColor(Color.BLACK);
+        dialPaint.setAntiAlias(true);
+        dialPaint.setPathEffect(new PathDashPathEffect(dialPathShape, ADVANCE, 0, PathDashPathEffect.Style.TRANSLATE));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(centerPoint.x, centerPoint.y, 10, circlePaint);
-        canvas.drawPath(dialPath, paint);
+        canvas.drawCircle(centerPoint.x, centerPoint.y, DIAL_RADIUS, dialPaint);
+
         super.onDraw(canvas);
     }
 }
